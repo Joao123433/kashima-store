7f0ecdff9b096d487dacba8a99f846fe34d30033
Joao123433 <joaosantos123433@gmail.com>
2023-10-21 17:15:41 -0300

    Terminando de configurar a view compra

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -7,5 +7,7 @@ class InicioController < ApplicationController
   def compra
     @compra = Clothing.find(params[:escolha])
     @proximos = @clotings.reject { |p| p == @compra }
+    @next = []
+    0.upto(2) { @next.push(@proximos.delete_at(Random.rand(@proximos.size))) }
   end
 end
